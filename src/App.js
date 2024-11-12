0fdc019abdcc061c4e8d6a2cd7d2d71f35e95f8d
pratham-chawdhry <122348596+pratham-chawdhry@users.noreply.github.com>
2024-11-13 03:46:12 +0530

    Commit-1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import Page3 from "./components/Page3";
-import Page2 from "./components/Page2";
-import Login from "./components/Login/Login";
-import Page1 from "./components/Page1";
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
+import Login from './components/Login/Login';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
 
-
-function App() {
+const App = () => {
   return (
     <Routes>
-      <Route path='/page1' element={<Page1 />}/>
-  <Route path='/login' element={<Login />}/>
-  <Route path='/page2' element={<Page2 />}/>
-  <Route path='/page3' element={<Page3 />}/>
-</Routes>
+      <Route path="/page1" element={<Page1 />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/page2" element={<Page2 />} />
+      <Route path="/page3" element={<Page3 />} />
+    </Routes>
   );
-}
+};
 
 export default App;
