ce3bbe070d2b11480d3ce8931ef3aec33fc4789f
KrishDave1 <Krish.Dave@iiitb.ac.in>
2024-11-14 15:15:39 +0530

    Added Api-example route

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import Page3 from "./components/Page3";
 import Page2 from "./components/Page2";
 import Login from "./components/Login/Login";
 import Page1 from "./components/Page1";
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-
+import ApiExample from "./Table/ApiExample";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route path='/page1' element={<Page1 />}/>
-  <Route path='/login' element={<Login />}/>
-  <Route path='/page2' element={<Page2 />}/>
-  <Route path='/page3' element={<Page3 />}/>
-</Routes>
+      <Route path='/page1' element={<Page1 />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/page2' element={<Page2 />} />
+      <Route path='/page3' element={<Page3 />} />
+      <Route path='/api-example' element={<ApiExample />} />
+    </Routes>
   );
 }
 
